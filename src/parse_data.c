acf4c632c033ef3563596239e2bb810390b7e8c8
home <aleksandr.tikka@gmail.com>
2022-06-27 15:10:45 +0300

    first draft of the architecture

--- src/parse_data.c
+++ src/parse_data.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 11:23:26 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/23 11:24:43 by altikka          ###   ########.fr       */
+/*   Updated: 2022/06/27 10:42:39 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 int	parse_data(t_filler *f)
 {
-	(void ) f;
+	ft_bzero(&(f->map.start), sizeof(f->map.start));
+	ft_bzero(&(f->map.end), sizeof(f->map.end));
+	if (parse_map(f) < 0)
+		return (panic(NULL, "Error: unable to parse map"));
+//	ft_bzero();
+//	ft_bzero();
+//	ft_bzero();
+//	if (parse_piece(f) < 0)
+//		return (panic(f, "Error: unable to parse piece"));
+//	if (parse_heatmap(f) < 0)
+//		return (panic(f, "Error: unable to parse heatmap"));
 	return (1);
 }
