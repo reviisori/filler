bf0a7eb00909a99b4014a83e282744a2629ba54a
home <aleksandr.tikka@gmail.com>
2022-06-27 15:11:30 +0300

    structure for parsing map data

---
+++ src/parse_map.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 10:06:36 by altikka           #+#    #+#             */
+/*   Updated: 2022/06/27 14:58:32 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "filler.h"
+
+int	parse_map(t_filler *f)
+{
+	// (f->turn < 3)
+	if (!f->map.arr)
+	{
+		if (set_grid_size(&(f->map.size), "Plateau") < 0)
+			return (panic(NULL, "Error: invalid map size"));
+		if (allocate_grid(&(f->map)) < 0)
+			return (panic(NULL, "Error: allocating map failed"));
+	}
+	else
+		skip_next_line();//skip Plateau: X Y
+	skip_next_line();//skip line with 01234...
+	if (populate_grid(&(f->map), ".oOxX", 4) < 0)
+		return (panic(f, "Error: couldn't populate map"));
+	if (set_grid_psoi(&(f->map)) < 0)
+		return (panic(f, "Error: couldn't determine points of interest"));
+	return (1);
+}
