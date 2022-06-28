46a10c4ce4190a37f2acf0105d5e4939c965d686
home <aleksandr.tikka@gmail.com>
2022-06-28 17:45:08 +0300

    added variable to function call
    better comments

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,11 +6,17 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 10:06:36 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/27 14:58:32 by altikka          ###   ########.fr       */
+/*   Updated: 2022/06/28 17:29:05 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+
+int	set_grid_psoi(t_grid *grid)
+{
+	(void ) grid;
+	return (1);
+}
 
 int	parse_map(t_filler *f)
 {
@@ -19,13 +25,16 @@ int	parse_map(t_filler *f)
 	{
 		if (set_grid_size(&(f->map.size), "Plateau") < 0)
 			return (panic(NULL, "Error: invalid map size"));
-		if (allocate_grid(&(f->map)) < 0)
+		if (allocate_grid(&(f->map), sizeof(char)) < 0)
 			return (panic(NULL, "Error: allocating map failed"));
 	}
 	else
-		skip_next_line();//skip Plateau: X Y
-	skip_next_line();//skip line with 01234...
-	if (populate_grid(&(f->map), ".oOxX", 4) < 0)
+		skip_next_line(); //skip 'Plateau: X Y'
+	skip_next_line(); //skip line with '    01234...'
+	ft_putstr_fd("TURN: ", 2);			//de
+	ft_putnbr_fd(f->turn, 2);			//bug
+	ft_putstr_fd("\n", 2);				//ging
+	if (populate_grid(&(f->map), ".ox", 4) < 0) //offset = 4 to skip '000 '
 		return (panic(f, "Error: couldn't populate map"));
 	if (set_grid_psoi(&(f->map)) < 0)
 		return (panic(f, "Error: couldn't determine points of interest"));
