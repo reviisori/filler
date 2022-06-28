fac927034ab9b62df4c4ce59c8d13d65847c1112
home <aleksandr.tikka@gmail.com>
2022-06-28 17:43:44 +0300

    added calls to free arrays

--- src/clean_up.c
+++ src/clean_up.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 10:30:33 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/24 11:15:50 by altikka          ###   ########.fr       */
+/*   Updated: 2022/06/28 11:44:40 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 void	free_data(t_filler *f)
 {
-	(void ) f;
+	if (f->map.arr)
+		ft_memdelarr(&(f->map.arr));
+	if (f->heatmap.arr)
+		ft_memdelarr(&(f->heatmap.arr));
+	if (f->piece.arr)
+		ft_memdelarr(&(f->piece.arr));
 }
 
 int	panic(t_filler *f, const char *msg)
