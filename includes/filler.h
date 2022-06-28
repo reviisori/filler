aa825dd52766d8b63feb039c06dcfd8a219620e7
home <aleksandr.tikka@gmail.com>
2022-06-28 20:08:17 +0300

    tweaked prototype parameters

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 09:35:27 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/28 18:00:33 by altikka          ###   ########.fr       */
+/*   Updated: 2022/06/28 20:05:47 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,9 @@ int		parse_data(t_filler *f);
 int		parse_map(t_filler *f);
 int		skip_next_line(void);
 int		set_grid_size(t_pos *size, char *needle);
-int		allocate_grid(t_grid *grid, int type);
+int		allocate_grid(t_grid *grid, size_t type);
 int		populate_grid(t_grid *grid, char *marks, int ofs);
-int		set_grid_psoi(t_filler *f);
+int		set_grid_psoi(t_grid *grid, char target);
 void	free_data(t_filler *f);
 int		place_piece(t_filler *f, t_pos *answer);
 
