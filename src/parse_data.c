84d397c89bd3a162a6ec67322240459d42ca4df8
Aleksandr Tikka <altikka@student.hive.fi>
2022-06-30 16:04:06 +0300

    added call to parse_piece function

--- src/parse_data.c
+++ src/parse_data.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 11:23:26 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/27 10:42:39 by altikka          ###   ########.fr       */
+/*   Updated: 2022/06/30 14:49:54 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ int	parse_data(t_filler *f)
 	ft_bzero(&(f->map.end), sizeof(f->map.end));
 	if (parse_map(f) < 0)
 		return (panic(NULL, "Error: unable to parse map"));
-//	ft_bzero();
-//	ft_bzero();
-//	ft_bzero();
-//	if (parse_piece(f) < 0)
-//		return (panic(f, "Error: unable to parse piece"));
+	ft_bzero(&(f->piece.start), sizeof(f->piece.start));
+	ft_bzero(&(f->piece.end), sizeof(f->piece.end));
+	if (parse_piece(f) < 0)
+		return (panic(f, "Error: unable to parse piece"));
 //	if (parse_heatmap(f) < 0)
 //		return (panic(f, "Error: unable to parse heatmap"));
 	return (1);
