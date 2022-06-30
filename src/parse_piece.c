2e7e5f2f005a1d9e503dc3ff81800083614a7cb7
Aleksandr Tikka <altikka@student.hive.fi>
2022-06-30 16:05:03 +0300

    function to parse piece data
    quite similar to parse_map with alot of functions reused

---
+++ src/parse_piece.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_piece.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 14:50:10 by altikka           #+#    #+#             */
+/*   Updated: 2022/06/30 16:00:18 by altikka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "filler.h"
+
+int	parse_piece(t_filler *f)
+{
+	if (set_grid_size(&(f->piece.size), "Piece") < 0)
+		return (panic(NULL, "Error: invalid piece size"));
+	if (allocate_grid(&(f->piece), sizeof(char)) < 0)
+		return (panic(NULL, "Error: allocating piece failed"));
+	if (populate_grid(&(f->piece), ".*", 0) < 0)
+		return (panic(f, "Error: couldn't populate piece"));
+	set_grid_psoi(&(f->piece), '*');
+	return (1);
+}
