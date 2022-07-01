d58f6074d91464e71311c7d2429c5f648a70aae5
Aleksandr Tikka <altikka@student.hive.fi>
2022-07-01 16:16:12 +0300

    heatmap data prints to the file

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:26:18 by altikka           #+#    #+#             */
-/*   Updated: 2022/07/01 12:54:56 by altikka          ###   ########.fr       */
+/*   Updated: 2022/07/01 15:02:42 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 int	debug(t_filler *f)
 {
 	FILE	*file;
-	char	**m;
-	char	**p;
+	char	**m, **p;
+	int		row, col;
+	int		nbr;
 
 	file = fopen("debug.txt", "w+");
 	if (!file)
@@ -26,7 +27,7 @@ int	debug(t_filler *f)
 	fprintf(file, "Our hero: %c | Opponent: %c |  Turn:  %d\n", f->player,
 			f->opponent, f->turn);
 	//map size & turn number:
-	fprintf(file, "Plateau [%d] [%d]            Piece [%d] [%d]\n", f->map.size.x,
+	fprintf(file, "Plateau: [%d,%d]   -&-   Piece: [%d,%d]\n", f->map.size.x,
 			f->map.size.y, f->piece.size.x, f->piece.size.y);
 	//map & piece:
 	m = (char **) f->map.arr;
@@ -44,11 +45,27 @@ int	debug(t_filler *f)
 		m++;
 	}
 	//map's points of interest:
-	fprintf(file, "Map psoi [%d][%d] [%d][%d] | ", f->map.start.x,
+	fprintf(file, "\t  [%d,%d] [%d,%d]  ", f->map.start.x,
 			f->map.start.y, f->map.end.x, f->map.end.y);
 	//piece's points of interest:
-	fprintf(file, "Piece psoi [%d][%d] [%d][%d]\n", f->piece.start.x,
+	fprintf(file, "  [%d,%d] [%d,%d]\n", f->piece.start.x,
 			f->piece.start.y, f->piece.end.x, f->piece.end.y);
+	//heatmap:
+	fprintf(file, "Heatmap:\n");
+	row = 0;
+	while (row < f->heatmap.size.x)
+	{
+		col = 0;
+		fprintf(file, "\t ");
+		while (col < f->heatmap.size.y)
+		{
+			nbr = *((int *) &f->heatmap.arr[row][col]);
+			fprintf(file, "%d", nbr);
+			col++;
+		}
+		fprintf(file, "\n");
+		row++;
+	}
 	if (fclose(file) == -1)
 		return (-1);
 	return (1);
