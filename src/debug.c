b6bf05055ebe0336e6e9cf8758c67bd56819996d
Aleksandr Tikka <altikka@student.hive.fi>
2022-07-01 12:55:36 +0300

    more readable output

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:26:18 by altikka           #+#    #+#             */
-/*   Updated: 2022/06/30 15:55:46 by altikka          ###   ########.fr       */
+/*   Updated: 2022/07/01 12:54:56 by altikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,31 @@ int	debug(t_filler *f)
 	if (!file)
 		return (-1);
 	//player info:
-	fprintf(file, "Our hero: %c | Opponent: %c\n", f->player, f->opponent);
+	fprintf(file, "Our hero: %c | Opponent: %c |  Turn:  %d\n", f->player,
+			f->opponent, f->turn);
 	//map size & turn number:
-	fprintf(file, "Plateau [%d] [%d] after turn: %d\n", f->map.size.x,
-			f->map.size.y, f->turn);
-	//map itself:
+	fprintf(file, "Plateau [%d] [%d]            Piece [%d] [%d]\n", f->map.size.x,
+			f->map.size.y, f->piece.size.x, f->piece.size.y);
+	//map & piece:
 	m = (char **) f->map.arr;
+	p = (char **) f->piece.arr;
 	while (*m)
 	{
-		fprintf(file, "\t %s\n", *m);
+		fprintf(file, "\t %s    ", *m);
+		if (*p)
+		{
+			fprintf(file, "%s\n", *p);
+			p++;
+		}
+		else
+			fprintf(file, "\n");
 		m++;
 	}
 	//map's points of interest:
-	fprintf(file, "    hi: [%d][%d] | low: [%d][%d]\n", f->map.start.x,
+	fprintf(file, "Map psoi [%d][%d] [%d][%d] | ", f->map.start.x,
 			f->map.start.y, f->map.end.x, f->map.end.y);
-	//piece size:
-	fprintf(file, "Piece [%d] [%d] to place:\n", f->piece.size.x,
-		f->piece.size.y);
-	//piece itself:
-	p = (char **) f->piece.arr;
-	while (*p)
-	{
-		fprintf(file, "\t %s\n", *p);
-		p++;
-	}
 	//piece's points of interest:
-	fprintf(file, "    hi: [%d][%d] | low: [%d][%d]\n", f->piece.start.x,
+	fprintf(file, "Piece psoi [%d][%d] [%d][%d]\n", f->piece.start.x,
 			f->piece.start.y, f->piece.end.x, f->piece.end.y);
 	if (fclose(file) == -1)
 		return (-1);
