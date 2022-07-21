8043b6e68d97abbea6c250615a6b7b6ee94f83df
home <aleksandr.tikka@gmail.com>
2022-07-21 17:00:43 +0300

    struggle continues
    Can add text or button but now both - looking into 'boxes'. Tried to add command to close windown when Esc is pressed - doesn't work

--- visualiser/src/main.rs
+++ visualiser/src/main.rs
@@ -1,5 +1,7 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Label};
+use gtk;
+use gtk::traits::*;
 
 fn main() {
     let app = Application::builder()
@@ -13,8 +15,23 @@ fn main() {
             .default_height(700)
             .title("Filler Visualiser")
             .build();
+        let title = Label::new(Some("Filler Logo\n\tand\nPlay Button"));
+            win.add(&title);
+        //  in order to add more than one widget
+        //  we need to create a box first
+
+        //let button = gtk::Button::with_label("PLAY");
+        //  win.add(&button);
 
         win.show_all();
+        // Presss 'Esc' to close windonw
+/*        win.connect_key_press_event(move |_, key| {
+            match key.keyval as i32 {
+                key::Escape => gtk::main_quit(),
+                _ => ()
+            }
+            gtk::signal::Inhibit(false)
+        }); */
     });
 
     app.run();
