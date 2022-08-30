e5f17e79a34fda63a5d171c607656a740fc77414
Aleksandr Tikka <altikka@student.hive.fi>
2022-08-30 17:10:11 +0300

    working function for getting map dimensions and drawing it

--- visualiser_py/main.py
+++ visualiser_py/main.py
@@ -6,7 +6,7 @@
 #    By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/25 14:26:19 by altikka           #+#    #+#              #
-#    Updated: 2022/08/29 16:37:35 by altikka          ###   ########.fr        #
+#    Updated: 2022/08/30 17:07:15 by altikka          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,6 +22,9 @@ window = pygame.display.set_mode((800, 800))
 background = pygame.Surface((800, 800))
 background.fill(pygame.Color('#becb11'))
 
+bg_test = pygame.Surface((800, 800))
+bg_test.fill(pygame.Color('#11cb7b'))
+
 ###load & scale images:
 logo_img = pygame.image.load("./resources/filler_logo.png").convert_alpha()
 logo_img = pygame.transform.scale(logo_img, (600, 160))
@@ -29,6 +32,8 @@ start_img = pygame.image.load("./resources/button_start.png").convert_alpha()
 start_img = pygame.transform.scale(start_img, (210, 90))
 start_d_img = pygame.image.load("./resources/button_start_down.png").convert_alpha()
 start_d_img = pygame.transform.scale(start_d_img, (210, 90))
+empty_img = pygame.image.load("./resources/sqr_empty.png").convert_alpha()
+empty_img = pygame.transform.scale(empty_img, (43, 43))# 44x44 for map00, 15x15 for map01 and 5x5 for map02
 
 
 ###button class:
@@ -51,12 +56,15 @@ class button():
         surface.blit(self.image, (self.rect.x,  self.rect.y))
         return mouse_action
 
-#
-#I get player data
-#II get board data
-#III draw the board
-#IV update the board with piece
-#
+##################################
+#                                #
+# I: get player data             #
+# II: get board data             #
+# III: draw the board            #
+# IV: get piece data             #
+# V: update the board and pieces #
+#                                #
+##################################
 
 def return_next_line(x):
     for _ in range(x):
@@ -65,26 +73,28 @@ def return_next_line(x):
     return str
 
 ###game loop:
+init_data = True
 def play():
     is_playing = True
+    global init_data
 
     ###I: player data
-    p1 = return_next_line(5)
-    p2 = return_next_line(1)
-    print(p1)
-    print(p2)
-    ###II: map data
-    plateau = return_next_line(1)
-    print(plateau)
-    map_size = plateau.rstrip(":\n").split(" ")
-    map_x = int(map_size[1])
-    map_y = int(map_size[2])
-    print(map_x, map_y)
+    if init_data:
+        p1 = return_next_line(5)
+        p2 = return_next_line(1)
+        print(p1)#needs better splitting and grebbing
+        print(p2)#^
 
+        ###II: map data
+        plateau = return_next_line(1)
+        print(plateau)
+        map_size = plateau.rstrip(":\n").split(" ")
+        map_x = int(map_size[1])
+        map_y = int(map_size[2])
+        print(map_x, map_y)
+        init_data = False
     while is_playing:
-
-        window.blit(background, (0, 0))
-
+        window.blit(bg_test, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
@@ -93,18 +103,35 @@ def play():
             if event.type == pygame.QUIT:
                 print("Exiting the game...")
                 is_playing = False
-    
-        pygame.display.update()
-        #print("Playing the game...")
 
-###variables:
+        ###III: drawing the map
+        sqr_side = 43
+        x_ofs = 10
+        y_ofs = 25
+        s = sys.stdin.readline()
+        x = 0
+        while x < map_x:
+            s = sys.stdin.readline()
+            y = 4
+            while y < map_y + 4:
+                window.blit(empty_img, (y_ofs, x_ofs))
+                y_ofs = y_ofs + sqr_side
+                #print(s[y], end=" ")#test
+                y += 1
+            x_ofs = x_ofs + sqr_side
+            y_ofs = 25
+            #print("\n")#test
+            x += 1
+        pygame.display.update()
+        pygame.time.wait(60)
+
+### button variables:
 start_button = button(295, 525, start_img)
 start_button_d = button(295, 525, start_d_img)
 
 ###event loop:
 is_running = True
 while is_running:
-
     window.blit(background, (0, 0))
     window.blit(logo_img, (100, 340))
     
@@ -113,9 +140,9 @@ while is_running:
         start_button_d.draw(window)
         pygame.display.update()
         pygame.time.wait(120)
-        print("PLAY")
 
         ###play the game:
+        print("PLAY")
         play()
 
     for event in pygame.event.get():
@@ -126,5 +153,4 @@ while is_running:
         if event.type == pygame.QUIT:
             print("Quitting...")
             is_running = False
-    
     pygame.display.update()
