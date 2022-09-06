afd0c5b9179a2c1df4b48f2b168a4b964aaea516
home <aleksandr.tikka@gmail.com>
2022-09-06 11:15:32 +0300

    all resources now scaleble with resolution
    added visual effect for the last placed pieces for both players

--- visualiser_py/main.py
+++ visualiser_py/main.py
@@ -6,7 +6,7 @@
 #    By: altikka <altikka@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/25 14:26:19 by altikka           #+#    #+#              #
-#    Updated: 2022/09/05 18:32:45 by altikka          ###   ########.fr        #
+#    Updated: 2022/09/06 10:40:06 by altikka          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -62,8 +62,8 @@ class button():
         return mouse_action
 
 ###button variables:
-start_button = button(295, 525, start_img)
-start_button_d = button(295, 525, start_d_img)
+start_button = button(resolution / 2 - 105, resolution / 2 + 125, start_img)
+start_button_d = button(resolution / 2 - 105, resolution / 2 + 125, start_d_img)
 
 ###function to load & scale map resources accordingly:
 def load_map_resources(sqr_side):
@@ -71,9 +71,13 @@ def load_map_resources(sqr_side):
     empty_img = pygame.transform.scale(empty_img, (sqr_side, sqr_side))
     o_img = pygame.image.load("./resources/sqr_o.png").convert_alpha()
     o_img = pygame.transform.scale(o_img, (sqr_side, sqr_side))
+    o_new = pygame.image.load("./resources/sqr_o_new.png").convert_alpha()
+    o_new = pygame.transform.scale(o_new, (sqr_side, sqr_side))
     x_img = pygame.image.load("./resources/sqr_x.png").convert_alpha()
     x_img = pygame.transform.scale(x_img, (sqr_side, sqr_side))
-    return empty_img, o_img, x_img
+    x_new = pygame.image.load("./resources/sqr_x_new.png").convert_alpha()
+    x_new = pygame.transform.scale(x_new, (sqr_side, sqr_side))
+    return empty_img, o_img, o_new, x_img, x_new
 
 ###function to init map data:
 def init_map_data(dim, res):
@@ -91,7 +95,7 @@ def init_map_data(dim, res):
         y = (resolution - sqr_side * 99) / 2
     return x, y, sqr_side 
 
-###function to quickly skip x ammount of line in stdin:
+###function to quickly skip x ammount of lines in stdin:
 def return_next_line(x):
     for _ in range(x):
         sys.stdin.readline()
@@ -124,7 +128,7 @@ def play():
     end = False
     game_speed = 50
 
-    ###I: get player data
+    ###I get player data:
     if init_data:
         p1 = return_next_line(5)
         p2 = return_next_line(1)
@@ -132,18 +136,16 @@ def play():
         p2_name = p2.split("/")
         print(p1_name[-1].split(".")[0], "vs.", p2_name[-1].split(".")[0])
 
-        ###II: get map data
+        ###II get map data:
         plateau = return_next_line(1)
         map_size = plateau.rstrip(":\n").split(" ")
         map_x = int(map_size[1])
         map_y = int(map_size[2])
         init_data = False
-        #print(plateau)
-        #print(map_x, map_y)
 
     while is_playing:
         x_ofs, y_ofs, sqr_side, = init_map_data(map_x, resolution)
-        empty_img, o_img, x_img = load_map_resources(sqr_side)
+        empty_img, o_img, o_new, x_img, x_new = load_map_resources(sqr_side)
         x_reset = x_ofs
         y_reset = y_ofs
 
@@ -152,20 +154,19 @@ def play():
         ###player names & game color:
         p1_font = pygame.font.Font(game_font, 18)
         p1_text = p1_font.render(p1_name[-1].split(".")[0], True, "#000000")
-        window.blit(p1_text, (y_reset + 70, x_reset - 50))
+        window.blit(p1_text, (y_reset + 75, x_reset - 50))
         p1_img = pygame.image.load("./resources/sqr_o.png").convert_alpha()
-        window.blit(p1_img, (y_reset + 55, x_reset - 44))
+        window.blit(p1_img, (y_reset + 60, x_reset - 44))
 
         p2_font = pygame.font.Font(game_font, 18)
         p2_text = p2_font.render(p2_name[-1].split(".")[0], True, "#000000")
-        #window.blit(p2_text, (resolution / 2 + 50, x_reset - 50))# needs calc
-        window.blit(p2_text, (y_reset + 70, x_reset - 25))# needs calc
+        window.blit(p2_text, (y_reset + 75, x_reset - 25))
         p2_img = pygame.image.load("./resources/sqr_x.png").convert_alpha()
-        #window.blit(p2_img, (resolution / 2 + 35, x_reset - 44))
-        window.blit(p2_img, (y_reset + 55, x_reset - 19))
+        window.blit(p2_img, (y_reset + 60, x_reset - 19))
 
-        ###V: exit the game
         s = sys.stdin.readline()
+
+        ###V exit the game:
         if end:
             print("O:", int(score_o))
             print("X:", int(score_x))
@@ -174,16 +175,20 @@ def play():
             is_playing = False
             return
 
-        ###III: draw the map
+        ###III draw the map:
         x = 0
         while x < map_x:
             s = sys.stdin.readline()
             y = 4
             while y < map_y + 4:
-                if s[y] == 'o' or s[y] == 'O':
+                if s[y] == 'O':
                     window.blit(o_img, (y_ofs, x_ofs))
-                elif s[y] == 'x' or s[y] == 'X':
+                elif s[y] == 'o':
+                    window.blit(o_new, (y_ofs, x_ofs))
+                elif s[y] == 'X':
                     window.blit(x_img, (y_ofs, x_ofs))
+                elif s[y] == 'x':
+                    window.blit(x_new, (y_ofs, x_ofs))
                 else:
                     window.blit(empty_img, (y_ofs, x_ofs))
                 y_ofs = y_ofs + sqr_side
@@ -192,19 +197,19 @@ def play():
             y_ofs = y_reset
             x += 1
 
-        ###IV: get piece data SKIPPED FOR NOW
+        ###IV get piece data to skip it:
         piece = return_next_line(0)
         piece_size = piece.rstrip(":\n").split(" ")
         piece_x = int(piece_size[1])
         piece_y = int(piece_size[2])
-        s = return_next_line(piece_x)#THE SKIP
+        s = return_next_line(piece_x)
 
         ###final scores:
-        s = return_next_line(0)#has '== 0 fin: ...' at the end
+        s = return_next_line(0)
         fin_o = s.split(" ")
         if "fin" in fin_o[2]:
             score_o = fin_o[3]
-            s = sys.stdin.readline()#has '== X fin: ...' at the end
+            s = sys.stdin.readline()
             fin_x = s.split(" ")
             if "fin" in fin_x[2]:
                 score_x = fin_x[3]
@@ -231,16 +236,13 @@ def play():
                     score = score_font.render(winner, True, "#000000")
                     print(p2_name[-1].split(".")[0], "WINS!")
                 score_y = (resolution / 2) - ((len(winner) * 24) / 2)
-
                 window.blit(score, (score_y, resolution - x_reset + 25))
                 end = True
 
-        ##react if one of the players can't play anymore
+        ##one of the players stopped playing:
         rip = s.rstrip(":\n").split(" ")
         if "Piece" in rip[0]:
             s = return_next_line(int(rip[1]) + 1)
-        #print(piece)
-        #print(piece_x, piece_y)
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -267,7 +269,7 @@ def play():
                 print("Exiting the game...")
                 is_playing = False
 
-        ###play, pause, fastforward test:
+        ###play and fastforward:
         if game_speed == 50 and not paused:
             window.blit(play_img, (y_reset, x_reset - 25))
         if game_speed == 10 and not paused:
@@ -292,7 +294,7 @@ is_running = True
 start = True
 while is_running:
     window.blit(bg_main, (0, 0))
-    window.blit(logo_img, (100, 340))
+    window.blit(logo_img, ((resolution - 600) / 2, resolution / 2 - 60))
     
     if start and start_button.draw(window):
         start_button_d.draw(window)
@@ -304,12 +306,11 @@ while is_running:
     if not start:
         by_font = pygame.font.Font(game_font, 18)
         by_who = by_font.render("by altikka", True, "#38c5f8")
-        #window.blit(by_who, (220, 510))
-        window.blit(by_who, (610, 10))#was 10, 10
+        window.blit(by_who, (10, 10))
         bye_font = pygame.font.Font(game_font, 16)
         bye = bye_font.render("Press 'Esc' to exit", True, "#000000")
         bye_y = (resolution / 2) - ((len("Press 'Esc' to exit") * 16) / 2)
-        window.blit(bye, (bye_y, 530))
+        window.blit(bye, (bye_y, resolution / 2 + 130))
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
